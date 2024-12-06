8d66fd180c4e4515af4da2c8abf7f2cf989e4ed0
juanmi30 <j.manriquec@uniandes.edu.co>
2024-12-06 01:39:03 -0500

    add 2

--- invoices_service/models/models.py
+++ invoices_service/models/models.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel, Field, ConfigDict
 from typing import List
 from models.db import PyObjectId
+from typing import Optional
 
 # Modelo de factura individual
 class Invoice(BaseModel):
-    id: str = Field(..., alias="_id")
+    id: Optional[str] = Field(None, alias="_id")
     institution_code: str = Field(..., description="Código de la institución")
     amount: float = Field(..., description="Monto total de la factura")
     date: str = Field(..., description="Fecha de emisión")
